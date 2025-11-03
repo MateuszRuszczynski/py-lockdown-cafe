7bb19feca9a62a722e26abe09fb55c5c7cb0ac9b
Mateusz <mateusz.ruszczynski@gmail.com>
2025-11-03 17:46:27 +0100

    Fixed issues from ai test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from .cafe import Cafe
-from .errors import (
+from app.cafe import Cafe
+from app.errors import (
     VaccineError,
     NotWearingMaskError
 )
@@ -11,20 +11,9 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
 
     for friend in friends:
         try:
-
             cafe.visit_cafe(friend)
-
         except VaccineError:
             return "All friends should be vaccinated"
-
-        except NotWearingMaskError:
-            pass
-
-    for friend in friends:
-        try:
-
-            cafe.visit_cafe(friend)
-
         except NotWearingMaskError:
             masks_to_buy += 1
 
